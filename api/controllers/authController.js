7466acdc2b750e1da68e12c645b32d82b561626e
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-14 08:45:28 +0530

    Creates register user

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -4,17 +4,36 @@ const asyncHandler = require('express-async-handler');
 
 // Register
 const register = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "User registers in successfully" });
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password)
+    return res.status(404).json({ message: 'All fields are required' });
+
+    // Hashing password
+    const hashPwd = await bcrypt.hash(password, 10);
+
+    // Create user object
+    const userObj = {
+        username,
+        email,
+        password: hashPwd
+    };
+
+    const user = await User.create(userObj);
+
+    if (!user) return res.status(500).json({ message: 'Something went wrong' });
+
+    res.status(200).json(user);
 });
 
 // Login
 const login = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "User logged in successfully" });
+  res.status(200).json({ message: 'User logged in successfully' });
 });
 
 // Logout
 const logout = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "User logged out successfully" });
+  res.status(200).json({ message: 'User logged out successfully' });
 });
 
 module.exports = { register, login, logout };
