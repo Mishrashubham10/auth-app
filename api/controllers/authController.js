3e4a3d62672295b54bfb68190f7fc7ff2b2cff98
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-14 17:09:04 +0530

    Creates login and logout functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -9,31 +9,54 @@ const register = asyncHandler(async (req, res) => {
   if (!username || !email || !password)
     return res.status(404).json({ message: 'All fields are required' });
 
-    // Hashing password
-    const hashPwd = await bcrypt.hash(password, 10);
+  // Hashing password
+  const hashPwd = await bcrypt.hash(password, 10);
 
-    // Create user object
-    const userObj = {
-        username,
-        email,
-        password: hashPwd
-    };
+  // Create user object
+  const userObj = {
+    username,
+    email,
+    password: hashPwd,
+  };
 
-    const user = await User.create(userObj);
+  const user = await User.create(userObj);
 
-    if (!user) return res.status(500).json({ message: 'Something went wrong' });
+  if (!user) return res.status(500).json({ message: 'Something went wrong' });
 
-    res.status(200).json(user);
+  res.status(201).json(user);
 });
 
 // Login
 const login = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'User logged in successfully' });
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res.status(404).json({ message: 'All fields are required' });
+
+  //   See if a user exists to login
+  const user = await User.findOne({ username });
+
+  if (!user) return res.status(404).json({ message: 'User not found' });
+
+  //   Match passwords of the users
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) return res.status(403).json({ message: 'Invalid credentials' })
+
+  res.status(200).json(`User with name of ${username} logged in`);
 });
 
 // Logout
 const logout = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'User logged out successfully' });
+  const { id } = req.params;
+
+  if (!id) return res.status(404).json({ message: 'Id is required!' });
+
+  const user = await User.findByIdAndDelete(id);
+
+  if (!user) return res.status(500).json({ message: 'User not found' });
+
+  res.status(200).json({ message: 'User deleted' });
 });
 
 module.exports = { register, login, logout };
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -4,6 +4,6 @@ const { register, login, logout } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout/:id', logout);
 
 module.exports = router;
